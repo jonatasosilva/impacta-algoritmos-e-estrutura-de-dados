50c692c9a30f28784f12917fa7de020b63bdf95b
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2022-04-10 13:55:22 -0300

    feat: adiciona insereMeio e removeMeio no menu

--- listaligada/UsaNo.java
+++ listaligada/UsaNo.java
@@ -29,9 +29,20 @@ class UsaNo {
                     ref.removeFim();
                     break;
                 case 5:
-                    ref.imprime();
+                    System.out.println("Digite o valor do novo item: ");
+                    int infoNovoItem = scanner.nextInt();
+                    System.out.println("Digite o valor do item específico: ");
+                    int infoItemEspecifico = scanner.nextInt();
+                    ref.insereMeio(infoNovoItem, infoItemEspecifico);
                     break;
                 case 6:
+                    System.out.println("Digite o valor do item: ");
+                    ref.removeMeio(scanner.nextInt());
+                    break;
+                case 7:
+                    ref.imprime();
+                    break;
+                case 8:
                     break;
             }
         } while (resposta != 6);
@@ -45,8 +56,10 @@ class UsaNo {
         System.out.println("2 - Remover item no início da lista");
         System.out.println("3 - Inserir item no final da lista");
         System.out.println("4 - Remover item no final da lista");
-        System.out.println("5 - Imprimir lista");
-        System.out.println("6 - Sair");
+        System.out.println("5 - Inserir item no meio da lista após item específico");
+        System.out.println("6 - Remover item no meio da lista após item específico");
+        System.out.println("7 - Imprimir lista");
+        System.out.println("8 - Sair");
     }
 
 }
